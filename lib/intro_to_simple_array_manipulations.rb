bd598d819424bb3bc4b0539af9d3d8edecfab94d
brookton <davidbrookton@gmail.com>
2019-04-14 14:53:38 -0600

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(arr, string)
+  return arr.push(string)
+end
+
+def using_unshift(arr,string)
+  return arr.unshift(string)
+end
+
+def using_pop(arr)
+  return arr.pop
+end
+
+def pop_with_args(arr)
+  return arr.pop(2)
+end
+
+def using_shift(arr)
+  return arr.shift
+end
+
+def shift_with_args(arr)
+  return arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  return arr1.concat(arr2)
+end
+
+def using_insert(arr, ele)
+  return arr.insert(4, ele)
+end
+
+def using_uniq(arr)
+  return arr.uniq
+end
+
+def using_flatten(arr)
+  return arr.flatten
+end
+
+def using_delete(arr, string)
+  return arr.delete(string)
+end
+
+def using_delete_at(arr, int)
+  return arr.delete_at(int)
+end
+  
+  
